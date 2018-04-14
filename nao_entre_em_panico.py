d80cfd9c3170573bae1a59cb4c9238d993509206
Jessica Temporal <jessicatemporal@gmail.com>
2018-04-14 10:26:44 -0300

    Adds API flask app

---
+++ nao_entre_em_panico.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def nao_entre_em_panico():
+    if request.headers.get('Authorization') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "Não entre em pânico!"})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
